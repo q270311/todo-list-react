352046f45d13a38c5fd0f68a9f8713f977c55b53
Michał Bałdyga <q270311@gmail.com>
2022-11-15 22:11:27 +0100

    Main App added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./Form";
+
+const  taskTable = [
+  {id:1, content: "zadanie 1", done: false},
+  {id:2, content: "zadanie 2", done: true},
+  {id:3, content: "zadanie 3", done: false},
+];
+const  hideDoneTask = false;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <h1 className="header">Lista zadań</h1>
+        <section className="section">
+            <div className="section__header">
+                Dodaj nowe zadanie
+            </div>
+            <Form />
+        </section>
+        <section className="section">
+            <div className="section__header">
+            </div>
+            <ul className="list">
+            </ul>
+        </section>
+    </div>
     </div>
   );
 }
