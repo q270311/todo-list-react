2a5f3b8b1f770f747434f9a4bc9b66bdfd14860a
Michał Bałdyga <q270311@gmail.com>
2022-11-16 21:48:29 +0100

    Tasks component used

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Form from "./Form";
+import Tasks from "./Tasks";
 
-const  taskTable = [
-  {id:1, content: "zadanie 1", done: false},
-  {id:2, content: "zadanie 2", done: true},
-  {id:3, content: "zadanie 3", done: false},
+const tasksTable = [
+  { id: 1, content: "zadanie 1", done: false },
+  { id: 2, content: "zadanie 2", done: true },
+  { id: 3, content: "zadanie 3", done: false },
 ];
-const  hideDoneTask = false;
+const hideDoneTasks = false;
 
 function App() {
   return (
@@ -13,18 +14,18 @@ function App() {
       <div className="main">
         <h1 className="header">Lista zadań</h1>
         <section className="section">
-            <div className="section__header">
-                Dodaj nowe zadanie
-            </div>
-            <Form />
+          <div className="section__header">
+            Dodaj nowe zadanie
+          </div>
+          <Form />
         </section>
         <section className="section">
-            <div className="section__header">
-            </div>
-            <ul className="list">
-            </ul>
+          <div className="section__header">
+            Lista zadań
+          </div>
+          <Tasks tasks={tasksTable} hideDoneTasks={hideDoneTasks} />
         </section>
-    </div>
+      </div>
     </div>
   );
 }
