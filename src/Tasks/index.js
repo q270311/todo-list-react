f3262037188cd6ef13d3810d579ff77c75f58d52
Michał Bałdyga <q270311@gmail.com>
2022-11-16 21:46:47 +0100

    Tasks element added

---
+++ src/Tasks/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+const Tasks = (props) => (
+   <ul className="list">
+      {props.tasks.map(task => (
+         <li className={`list__item ${task.done && props.hideDoneTasks ? " list__item--hidden" : ""}`}>
+            <button className="list__icon list__icon--green">{task.done ? "✔" : ""} </button>
+            <span className={`list__taskText ${task.done ? " list__taskText--done" : ""}`}>{task.content}</span>
+            <button className="list__icon list__icon--red"> 🗑 </button>
+         </li>))}
+   </ul >
+);
+
+export default Tasks;
