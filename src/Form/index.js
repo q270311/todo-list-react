8258342680ab9b24f2048a52ea7fe3a12d876f2c
Michał Bałdyga <q270311@gmail.com>
2022-11-19 21:39:29 +0100

    Chanege autofocus to autoFocus in input

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Form = () => (
     <form className="form">
-        <input type="text" autofocus placeholder="Co jest do zrobienia?" className="form__text" />
+        <input type="text" autoFocus placeholder="Co jest do zrobienia?" className="form__text" />
         <input type="button" value="Dodaj zadanie" className="form__button" />
     </form>
 );
