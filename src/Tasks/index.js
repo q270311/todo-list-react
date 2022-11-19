2cf57bde80dd374329949fd16754482bc8c583ae
Michał Bałdyga <q270311@gmail.com>
2022-11-19 21:40:48 +0100

    Change visualisation of cone

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,13 +3,20 @@ import "./style.css";
 const Tasks = (props) => (
    <ul className="list">
       {props.tasks.map(task => (
-         <li key={props.tasks.id}
-            className={`list__item ${task.done && props.hideDoneTasks ? " list__item--hidden" : ""}`}>
-            <button className="list__icon list__icon--green">{task.done ? "✔" : ""} </button>
-            <span className={`list__taskText ${task.done ? " list__taskText--done" : ""}`}>{task.content}</span>
-            <button className="list__icon list__icon--red"> 🗑 </button>
-         </li>))}
+         <li
+            key={task.id}
+            className={`list__item ${task.done && props.hideDoneTasks ? "list__item--hidden" : ""}`}
+         >
+            <button className="list__icon list__icon--green">
+               {task.done ? "✔" : ""}
+            </button>
+            <span className={`list__taskText ${task.done ? " list__taskText--done" : ""}`}>
+               {task.content}
+            </span>
+            <button className="list__icon list__icon--red">🗑</button>
+         </li>
+      ))}
    </ul >
 );
 
-export default Tasks;
+export default Tasks; 
